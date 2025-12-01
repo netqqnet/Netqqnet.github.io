551314dfea23ac99435d80b0529ddeb1d59f1d08
Ericlin <qing606@gmail.com>
2025-12-01 12:45:40 +0800

    feat(analytics): 添加谷歌分析并实现百度统计路由追踪
    添加谷歌分析配置项GTM-5DR52V到站点分析配置中
    在TeekLayoutProvider组件中实现百度统计的路由变化追踪功能

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -54,15 +54,21 @@ const teekConfig = defineTeekConfig({
         name: "蜀ICP备2025135343号",
         link: "http://beian.miit.gov.cn/",
       },
-      siteAnalytics: [
-      {
-        provider: "baidu",
-        options: {
-          id: "dc047fc743d8342d826086208bb74ab2",
-        },
-      },
-    ],
     },
+    siteAnalytics: [
+    {
+      provider: "baidu",
+      options: {
+        id: "dc047fc743d8342d826086208bb74ab2",
+      },
+    },
+    {
+      provider: "google",
+      options: {
+        id: "GTM-5DR52V",
+      },
+    },
+  ],
 });
 const description = ["不仅仅是万一免五，持续更新的投资学习站,聚合多家优质券商，帮你寻找最优的交易费率"].toString();
 
